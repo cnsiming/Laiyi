34152d9cc95aa025e374fa5d4ad2806c25debab5
SimonSi <SimonSi@SimonSirMBP.local>
2015-03-21 11:04:20 +0800

    Initial Commit

---
+++ Laiyi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laiyi
+//
+//  Created by SimonSi on 15-03-21.
+//  Copyright (c) 2015年 Zhejiang Laiyi Medicine Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
